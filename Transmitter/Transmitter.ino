125f179f0fe63f70649d43b6cdd945f0c9a94675
Jakub Gałek <49043385+jakubgalek@users.noreply.github.com>
2025-04-13 14:55:21 +0200

    Low energy code for Transmitter

--- Transmitter/Transmitter.ino
+++ Transmitter/Transmitter.ino
@@ -2,45 +2,50 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <Adafruit_BME280.h>
+#include <LowPower.h>
+#include <avr/power.h>   // Turn off unnecessary peripherals
 
 #define ONE_WIRE_BUS 2       // DS18B20
-
 #define PIN_TRANSMITER 11
 
 RH_ASK driver(2000, 10, PIN_TRANSMITER, 5);
-
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 Adafruit_BME280 bme;  // I2C
 
-
 void setup() {
-  Serial.begin(9600);
-  if (!driver.init())
-  Serial.println("RF 433 init failed!");
-  sensors.begin();   // DS18B20 init
-  if (!bme.begin(0x76)) 
-  Serial.println("Could not find a valid BME280!");
+  // Serial.begin(9600);
+  power_usart0_disable();
+
+  if (!driver.init()) {
+    // Serial.println("RF 433 init failed!");
+  }
+  
+  sensors.begin(); // DS18B20 init
+  if (!bme.begin(0x76)) {
+    // Serial.println("Could not find a valid BME280!");
+  }
 }
 
 void loop() {
-
-  // Request for temperatures
-  sensors.requestTemperatures();         
+  // Request for temperature from DS18B20
+  sensors.requestTemperatures();
 
   float temperature = sensors.getTempCByIndex(0);
 
   float humidity = bme.readHumidity();
 
-  // Create message about sesors data
-  String msg = String(temperature) + "|" + String(humidity);
+  // Create message about sensors data
+  String msg = String(temperature, 2) + "|" + String(humidity, 2);
 
-  Serial.print("Sending message: ");
-  Serial.println(msg);
+  //Serial.print("Sending message: ");
+  //Serial.println(msg);
 
   // Send RF message
   driver.send((uint8_t *)msg.c_str(), msg.length());
   driver.waitPacketSent();
 
-  delay(2000);  // Wait 2 seconds before sending next data
+  // Sleep for 8 seconds
+  LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+
 }
